e3c451768c63ca5c53556634c356050f73322903
Prithivraj Saravanan <prithivisaravanan069@gmail.com>
2024-09-25 02:22:25 +0000

    using prompt

--- 01-js-basics/01-data.js
+++ 01-js-basics/01-data.js
@@ -1,3 +1,19 @@
 
+const prompt = require("prompt-sync")();
 
-console.log("hello world");
+let name = prompt("enter the name: ");
+
+console.log(name);
+
+let n = 42 + "";
+console.log(typeof(n));
+
+
+
+let length = Number(prompt("enter the length: "));
+let width = Number(prompt("enter the width: "));
+
+let perimeter = (length + width) * 2;
+console.log("perimeter of rectangle: ", perimeter);
+
+console.log(Math.max(1,2));
